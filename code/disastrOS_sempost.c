758f7d8f411f376d223d522e6e29084ff8023aff
FrancescoNesci97 <francesconesci97@hotmail.it>
2019-10-21 17:47:21 +0200

    implementing the sem_post

--- code/disastrOS_sempost.c
+++ code/disastrOS_sempost.c
@@ -6,6 +6,24 @@
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
 
-void internal_semPost(){
-  // do stuff :)
+void internal_semClose(){
+
+	int semnum = running->syscall_args[0];
+	
+	SemDescriptor* sem_des =  SemDescriptorList_byFd(&(running->sem_descriptors),semnum);
+	SemDescriptorPtr* sem_des_ptr = sem_des->ptr;
+	
+	List_detach(&(sem_des->semaphore->descriptors),sem_des_ptr);
+		
+	SemDescriptorPtr_free(sem_des_ptr);
+	
+	List_detach(&(running->sem_descriptors),(ListItem*)sem_des);
+	
+	SemDescriptor_free(sem_des);
+
+	
+	
+	
+	
+  
 }
