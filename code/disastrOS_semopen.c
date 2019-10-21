0b3ee5e19d2bdda387125237b85359be92eff832
FrancescoNesci97 <francesconesci97@hotmail.it>
2019-10-21 12:37:10 +0200

    completing the sem_open

--- code/disastrOS_semopen.c
+++ code/disastrOS_semopen.c
@@ -11,14 +11,24 @@ void internal_semOpen(){
 
 int semnum = running->syscall_args[0];
 
+Semaphore* sem = SemaphoreList_byId((SemaphoreList*)&semaphores_list,semnum);
 
-
-
-
-
-
-
+if(!sem){
+	Semaphore* sem = Semaphore_alloc(semnum,1);
+	List_insert(&semaphores_list,semaphores_list.last,(ListItem*)sem);
 	
+}
+	
+SemDescriptor* sem_des = SemDescriptor_alloc(running->last_sem_fd,sem,running);
 
+running->last_sem_fd = running->last_sem_fd+1; 
+	
+SemDescriptorPtr* sem_des_ptr = SemDescriptorPtr_alloc(sem_des);
+
+List_insert(&(running->sem_descriptors),running->sem_descriptors.last,(ListItem*)sem_des);
+List_insert(&(sem->descriptors),sem->descriptors.last,(ListItem*)sem_des_ptr);
+
+running->syscall_retvalue = sem_des->fd;
+return;
 
 }
