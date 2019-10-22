a4f14e25ce7eebdc1f3ba77d3fa7ba5fa810f3b9
FrancescoNesci97 <francesconesci97@hotmail.it>
2019-10-22 14:32:47 +0200

    fixing some errors in the semaphore syscalls

--- code/disastrOS_sempost.c
+++ code/disastrOS_sempost.c
@@ -5,31 +5,40 @@
 #include "disastrOS_syscalls.h"
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
-void internal_semPost(){
 
+void internal_semPost(){
+printf("entro nella sempost!!!!!\n");
 int semnum = running->syscall_args[0];
 
 SemDescriptor* sem_des = SemDescriptorList_byFd(&(running->sem_descriptors),semnum);
 
-
+int count = sem_des->semaphore->count;
+printf("descriptor trovato!\n");
 //i suppose to implement a binary semaphore,so i don't need to increase the semaphore in that case
 if(count==1){
+	printf("non ci devo entrare\n");
 	running->syscall_retvalue = 0;
 }
 //in that case we'll have a sempahore under 0  and  we'll increase the semaphore and resume a waiting process
 else{
+	if(sem_des->semaphore->waiting_descriptors.size==0){
+		sem_des->semaphore->count = count+1;
+		running->syscall_retvalue = 0;
+		return;
+	}
+		
 	
-  ListItem* sem_des_ptr = List_detach(&(sem_des->semaphore->waiting_descriptors),&(sem_des->semaphore->waiting_descriptors.first);
-	
-	PCB* to_resume = (SemDescriptorPtr*)sem_des_ptr->descriptor->pcb;
+     	ListItem* sem_des_ptr = List_detach(&(sem_des->semaphore->waiting_descriptors),sem_des->semaphore->waiting_descriptors.first);
+	printf("successo\n");
+	PCB* to_resume = ((SemDescriptorPtr*)sem_des_ptr)->descriptor->pcb;
 
-	ListItem* ctrl = List_detatch(&(waiting_list),(ListItem*)to_resume);
-	ctrl = insert(&(ready_list),(ListItem*)to_resume);
+	/*ListItem* ctrl = */List_detach(&(waiting_list),(ListItem*)to_resume);
+	/*ctrl = */List_insert(&(ready_list),ready_list.last,(ListItem*)to_resume);
 	to_resume->status = Ready;
 	running->syscall_retvalue = 0;
 	
-	int count = sem_des->semaphore->count;
-  sem_des->semaphore->count = count+1;
+	
+        sem_des->semaphore->count = count+1;
 
 	
 
--- code/disastrOS_semwait.c
+++ code/disastrOS_semwait.c
@@ -1,9 +1,12 @@
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "disastrOS.h"
+#include "disastrOS_syscalls.h"
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
 
-
 void internal_semWait(){
- 
 
 
 int semnum = running->syscall_args[0];
@@ -15,16 +18,20 @@ if(count==1){
 	running->syscall_retvalue = 0;
 	sem_des->semaphore->count = count-1;
 }
-//in that case we can't return immediately from the syscall,we need to put the process in waiting
+//in that case we can't return immediately from the syscall,we need to put the process in waiting 
 else{
 
 
-SemDescriptorPtr* sem_des_ptr = SemDescriptorPtr_alloc(SemDescriptor* sem_des);
+SemDescriptorPtr* sem_des_ptr = SemDescriptorPtr_alloc(sem_des);
 List_insert(&(sem_des->semaphore->waiting_descriptors),sem_des->semaphore->waiting_descriptors.last,(ListItem*)sem_des_ptr);
 
 PCB* to_wait = sem_des->pcb;
 
 to_wait->status = Waiting;
+
+PCB* to_resume = (PCB*)List_detach(&ready_list,ready_list.first);
+
+running = to_resume;
 
 running->syscall_retvalue = 0;
 sem_des->semaphore->count = count-1;
@@ -35,5 +42,5 @@ sem_des->semaphore->count = count-1;
 return;
 
 
-
+  
 }
